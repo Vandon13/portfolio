78e6fa95f8deb8893db6f172dbbb77d433966558
Vandon13 <vandon0013@gmail.com>
2020-08-21 18:10:32 +0600

    bugs fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
-new WOW().init();
 var mySwiper = new Swiper(".swiper-container", {
   pagination: {
     el: " .projects-pagination",
     bulletClass: "projects-bullet",
     bulletActiveClass: "projects-bullet_active",
     clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
   },
 });
